184eb24e37403096e052c2ccecfc42e7cb830bed
lukwes-c <125727824+lukwes-c@users.noreply.github.com>
2025-04-18 21:03:11 +0200

    Update LandingPage.jsx

--- LandingPage.jsx
+++ LandingPage.jsx
@@ -1,4 +1,4 @@
-import { Button } from "./components/ui/button";
+import { Button } from "./components/ui/button.jsx";
 import { Card, CardContent } from "./components/ui/card";
 import { Check, Home, Mail, Lightbulb } from "lucide-react";
 
