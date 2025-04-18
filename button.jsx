f1799fda50cf7cd7aa3593b02eac62efb1e7c7d0
lukwes-c <125727824+lukwes-c@users.noreply.github.com>
2025-04-18 21:00:22 +0200

    Update Button.jsx

--- button.jsx
+++ button.jsx
@@ -1,3 +1,3 @@
 export function Button({ children, className }) {
-  return <button className={`bg-copper text-white rounded-xl ${className}`}>{children}</button>;
+  return <Button className={`bg-copper text-white rounded-xl ${className}`}>{children}</button>;
 }
